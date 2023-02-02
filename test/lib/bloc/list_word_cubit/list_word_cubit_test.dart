23eecf4a5e588c78dade293794e53424d342223c
o_gkmn <ozgur.gokmen735@gmail.com>
2023-02-02 21:24:13 +0300

    ListWordCubit tests has been updated

--- test/lib/bloc/list_word_cubit/list_word_cubit_test.dart
+++ test/lib/bloc/list_word_cubit/list_word_cubit_test.dart
@@ -1,34 +1,69 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:equatable/equatable.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_random_word/bloc/list_word_cubit/list_word_cubit.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:word_repository/word_repository.dart';
 
-import '../../../packages/word_repository/word_repository_test.dart';
-import '../../../packages/word_repository/word_repository_test.mocks.dart';
+class MockWordRepository extends Mock implements WordRepository {}
 
 void main() {
   late ListWordCubit listWordCubit;
-  MockMockWordApi mockApi;
-  WordRepository testRepo;
+  late MockWordRepository mockWordRepository;
 
-  group("ListWordCubit test", () {
-    setUp(() {
-      EquatableConfig.stringify = true;
-      mockApi = MockMockWordApi();
-      testRepo = WordRepository(wordApi: mockApi);
-      listWordCubit = ListWordCubit(testRepo);
-      when(testRepo.getWords()).thenAnswer((_) async => words);
-    });
+  setUp(() {
+    mockWordRepository = MockWordRepository();
+    listWordCubit = ListWordCubit(mockWordRepository);
+  });
 
-    blocTest<ListWordCubit, ListWordState>("=>List Word initialized",
-        build: () => listWordCubit,
-        act: (bloc) => listWordCubit.initialListWord(),
-        expect: () => [ListWordLoaded(words: words)]);
+  group("initialListWord", () {
+    const List<Word> words = [
+      Word(id: 0, englishWord: "englishWord_", turkishWord: "turkishWord_")
+    ];
 
-    tearDown(() {
-      listWordCubit.close();
-    });
+    const List<Word> emptyWords = [];
+    Exception texception = Exception();
+
+    blocTest<ListWordCubit, ListWordState>(
+      "emits [ListStatus.succed] when initialListWord is called succesfully",
+      setUp: () => when(() => mockWordRepository.getWords())
+          .thenAnswer((invocation) async => words),
+      build: () => listWordCubit,
+      act: (cubit) => cubit.initialListWord(),
+      expect: () =>
+          [const ListWordState(status: ListStatus.succed, words: words)],
+      verify: (cubit) {
+        verify(() => mockWordRepository.getWords()).called(1);
+      },
+    );
+
+    blocTest(
+      "emits [ListStatus.empty] when initialListWord is called succesfully",
+      setUp: () => when(() => mockWordRepository.getWords())
+          .thenAnswer((invocation) async => emptyWords),
+      build: () => listWordCubit,
+      act: (cubit) => listWordCubit.initialListWord(),
+      expect: () =>
+          <ListWordState>[const ListWordState(status: ListStatus.empty)],
+      verify: (bloc) {
+        verify(() => mockWordRepository.getWords()).called(1);
+      },
+    );
+
+    blocTest(
+      "emits [ListStatus.failure] when initialListWord is failure",
+      setUp: () =>
+          when(() => mockWordRepository.getWords()).thenThrow(texception),
+      build: () => listWordCubit,
+      act: (cubit) => listWordCubit.initialListWord(),
+      expect: () => <ListWordState>[
+        ListWordState(status: ListStatus.failure, exception: texception)
+      ],
+      verify: (bloc) {
+        verify(() => mockWordRepository.getWords()).called(1);
+      },
+    );
+  });
+  tearDown(() {
+    listWordCubit.close();
   });
 }
