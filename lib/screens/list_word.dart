57d80d08874e0ab068aefdd9625944bcd2c5372b
o_gkmn <ozgur.gokmen735@gmail.com>
2023-02-26 15:41:38 +0300

    word search feature is added

--- lib/screens/list_word.dart
+++ lib/screens/list_word.dart
@@ -44,7 +44,14 @@ class _ListWordBody extends StatelessWidget {
               padding: EdgeInsets.all(8.0),
               child: Icon(Icons.filter_list_rounded),
             ),
-          )
+          ),
+          IconButton(
+              onPressed: () => showSearch(
+                    context: context,
+                    delegate: _WordsListSearchDelegate(
+                        words: context.read<ListWordCubit>().wordsList),
+                  ),
+              icon: const Icon(Icons.search))
         ],
         title: Text(
           "Kelime Listesi",
@@ -106,6 +113,122 @@ class _ListWordBody extends StatelessWidget {
             default:
               return const Center(child: CircularProgressIndicator());
           }
+        },
+      ),
+    );
+  }
+}
+
+class _WordsListSearchDelegate extends SearchDelegate {
+  final List<Word> words;
+
+  _WordsListSearchDelegate({required this.words});
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return Theme.of(context).copyWith(
+      scaffoldBackgroundColor: Theme.of(context).scaffoldBackgroundColor,
+    );
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: const Icon(Icons.clear)),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<Word> matchQuery = [];
+    for (Word word in words) {
+      if (word.englishWord.toLowerCase().contains(query.toLowerCase()) ||
+          word.turkishWord.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(word);
+      }
+    }
+    return Container(
+      padding: const EdgeInsets.only(left: 3.0, right: 3.0),
+      child: ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: Theme.of(context).colorScheme.onPrimary,
+            margin:
+                const EdgeInsets.only(bottom: 3, left: 3.0, right: 3.0, top: 7),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: ListTile(
+              title: Text(
+                matchQuery[index].englishWord,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              subtitle: Text(
+                matchQuery[index].turkishWord,
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              onTap: () => Navigator.pushNamed(
+                context,
+                updateWord,
+                arguments: matchQuery.elementAt(index),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<Word> matchQuery = [];
+    for (Word word in words) {
+      if (word.englishWord.toLowerCase().contains(query.toLowerCase()) ||
+          word.turkishWord.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(word);
+      }
+    }
+    return Container(
+      padding: const EdgeInsets.only(left: 3.0, right: 3.0),
+      child: ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: Theme.of(context).colorScheme.onPrimary,
+            margin:
+                const EdgeInsets.only(bottom: 3, left: 3.0, right: 3.0, top: 7),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: ListTile(
+              title: Text(
+                matchQuery[index].englishWord,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              subtitle: Text(
+                matchQuery[index].turkishWord,
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              onTap: () => Navigator.pushNamed(
+                context,
+                updateWord,
+                arguments: matchQuery.elementAt(index),
+              ),
+            ),
+          );
         },
       ),
     );
